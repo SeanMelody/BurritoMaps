2bde71ba14d093d1da05a2490530bfd35551bf01
Sean Melody <seandmelody@gmail.com>
2021-05-13 12:42:08 -0700

    userControllers added

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+module.exports = {
+    users: (req, res) => {
+        res.send("Users will go here")
+    },
+    register: (req, res) => {
+        res.send("Registering will go here")
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
+const { users } = require("../controllers/userController")
+const { register } = require("../controllers/userController")
 
+// Test route unused later
 router.get("/test", (req, res) => {
     res.send("test route")
 })
 
-// Base route using "users" path from server.js
-router.get("/", (req, res) => {
-    res.send("connected")
-})
+// Base route using "users" path from server.js, and the test from userController
+router.get("/", users)
+
+router.post("/register", register)
+
 
 module.exports = router;
