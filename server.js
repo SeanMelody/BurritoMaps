70a663944567ec955204dd720f7611af900b189c
Sean Melody <seandmelody@gmail.com>
2021-05-13 12:20:56 -0700

    server file finished up

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose")
-const path = require("path")
+const path = require("path");
+const { isRegExp } = require("util");
 
 // const db = require("./models");
 // require("dotenv").config();
@@ -16,10 +17,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-// Syncing our sequelize models and then starting our Express app
-// db.sequelize.sync({ force: true }).then(() => {
-//     app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
-// });
+// Using Mongoose for MongoDB, burrito-eater
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/burrito-eater",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    },
+    (err) => {
+        if (err) throw err;
+        console.log("connected to mongodb")
+    }
+);
+// More Heroku optimization
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static("client/build"))
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
 
 // Let the user know the server is running, and which port.  Yeay!
 app.listen(PORT, () => {
