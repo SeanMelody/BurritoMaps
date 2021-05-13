5615ca8198fbef883335d31ef8eb829c55ed447a
Sean Melody <seandmelody@gmail.com>
2021-05-13 13:14:49 -0700

    React Router Dom added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./Pages/Login"
 
 function App() {
   return (
     <div className="App">
       <h1>Burrito Maps!</h1>
+      <Router>
+        <Switch>
+          <Route />
+          <Route />
+          <Route />
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ client/src/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <h1> hi from home</h1>
+        </div>
+    )
+}
