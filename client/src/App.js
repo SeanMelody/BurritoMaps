513242f306845227a6fa640af84de2b5f73fb8a0
Sean Melody <seandmelody@gmail.com>
2021-05-13 18:04:00 -0700

    React pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Login from "./Pages/Login"
+import Register from "./Pages/Register"
+import Home from "./Pages/Home"
 
 function App() {
   return (
@@ -8,9 +10,9 @@ function App() {
       <h1>Burrito Maps!</h1>
       <Router>
         <Switch>
-          <Route />
-          <Route />
-          <Route />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/" component={Home} />
         </Switch>
 
       </Router>
--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -7,3 +7,6 @@ export const Home = () => {
         </div>
     )
 }
+
+
+export default Home
