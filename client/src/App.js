7c14c570efe36b07a6764e12a9044133bf23fff1
Sean Melody <seandmelody@gmail.com>
2021-05-18 16:25:50 -0700

    Burrito Favicon Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { useEffect, useState } from "react"
+import Nav from "./Components/Nav"
 import Login from "./Pages/Login"
 import Register from "./Pages/Register"
 import Home from "./Pages/Home"
 
 function App() {
+
+  useEffect(() => {
+    console.log("hello")
+  }, [])
+
   return (
     <div className="App">
+      <Nav />
       <h1>Burrito Maps!</h1>
       <Router>
         <Switch>
--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
     return (
         <div>
-            <h1>Login Page</h1>
+            <h1>Hell from Login Page</h1>
         </div>
     )
 }
