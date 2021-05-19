2785e018951eaea62b963088ce3a69dd23185901
Sean Melody <seandmelody@gmail.com>
2021-05-18 17:04:43 -0700

    form set up for register

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -2,9 +2,18 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Hell from Login Page</h1>
-        </div>
+        <div className="container">
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                console.log("submitted")
+            }}>
+                <h3 className="col-md-12">Email:</h3>
+                <input type="text" name="email" className="row col-md-12"></input>
+                <h3 className="col-md-12">Password:</h3>
+                <input type="text" name="password" className="row col-md-12"></input>
+                <input type="submit" className="btn btn-secondary margin10" />
+            </form>
+        </div >
     )
 }
 
--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
 
 export const Register = () => {
+
+    const [form, setForm] = useState()
+
+    const onChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const submit = (e) => {
+        e.preventDefault()
+        console.log(form)
+    }
+
+
     return (
-        <div>
-            <h1> hi from Register</h1>
-        </div>
+        <div className="container">
+            <form onSubmit={submit}>
+                <h3 className="col-md-12">Email:</h3>
+                <input onChange={onChange} type="text" name="email" className="row col-md-12"></input>
+
+                <h3 className="col-md-12">Password:</h3>
+                <input onChange={onChange} type="text" name="password" className="row col-md-12"></input>
+
+                <h3 className="col-md-12">Password Check:</h3>
+                <input onChange={onChange} type="text" name="password" className="row col-md-12"></input>
+
+                <h3 className="col-md-12">Display Name:</h3>
+                <input onChange={onChange} type="text" name="password" className="row col-md-12"></input>
+
+                <input type="submit" className="btn btn-secondary margin10" />
+            </form>
+        </div >
     )
 }
 
